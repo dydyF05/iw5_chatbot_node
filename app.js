d41e75012a58d80f9b6291b89969cade1b6f94a8
dydy <dylanfoster.esgi@gmail.com>
2017-09-12 16:30:04 +0200

    bot now automatically displays the received msg, it's length and the dialogData

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const restify = require('restify')
 const server = restify.createServer()
 server.listen(process.env.port || 3978, () => {
     console.log(`server name: ${server.name} | server url: ${server.url}`)
+    console.log(`APP_ID: ${process.env.APP_ID} | APP_PASSWORD: ${process.env.APP_PASSWORD}`)
 })
 
 const connector = new builder.ChatConnector({
@@ -13,3 +14,9 @@ const connector = new builder.ChatConnector({
 
 server.post('api/messages', connector.listen())
 
+const bot = new builder.UniversalBot(connector, (session) => {
+    session.send(`it's working !`)
+    session.send(`msg: ${session.message.text}`)
+    session.send(`msg length: ${session.message.text.length}`)
+    session.send(`DialogData: ${JSON.stringify(session.dialogData)}`)
+})
